12bc20b38f8c9ef43e0bdc0258933809bd9ad3c2
Tetiana Deineka <tata4ka666@i.ua>
2023-12-14 16:39:08 +0200

    Update script3.js

--- HomeWork/js/script3.js
+++ HomeWork/js/script3.js
@@ -2,15 +2,24 @@
 // У вас є масив об’єктів fruts, і в кожному з них є name
 // Напишіть код, який перетворює їх в масив імен.
 
-// const fruts = [
+// const fruits = [
 //   { id: 0, name: "Apple" },
 //   { id: 1, name: "Tomat" },
 //   { id: 2, name: "Cherry" },
 //   { id: 3, name: "Orange" },
 // ];
 
+// let result = fruits.map((item) => item.name);
+// console.log(result);
+
 //  -- 2 --
 //Виведіть парні числа від 2 до 10, використовуючи цикл for.
+
+// let result=[];
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) result.push(i);
+// }
+// console.log(result);
 
 //  -- 3 --
 //Замініть цикл "for" на "while"
@@ -18,11 +27,34 @@
 //     console.lpg( `цифра ${i}!` );
 //   }
 
+// let i = 0
+// while (i < 5) {
+//     i++;
+//     console.log(`цифра ${i}!`);
+//   }
+
 //  -- 4 --
 //Напишіть цикл, який пропонує prompt ввести число більше за 100.
 //Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
 //Цикл повинен запитувати число доти, доки відвідувач не введе число,
 // більше за 100, або не скасує ввід/введе порожній рядок.
+
+// while (true) {
+//   let userInput = prompt(
+//     "Будь ласка, введіть число більше за 100 (або натисніть Cancel для виходу):"
+//   );
+//   if (userInput === null || userInput === "") {
+//     alert("Ви скасували введення. Завершення програми.");
+//     break;
+//   }
+//   let number = parseFloat(userInput);
+//   if (!isNaN(number) && number > 100) {
+//     alert("Ви ввели число більше за 100. Завершення програми.");
+//     break;
+//   } else {
+//     alert("Ви ввели число менше або дорівнює 100. Спробуйте ще раз.");
+//   }
+// }
 
 //  -- 5 --
 // Вирахуйте середній вік
@@ -33,3 +65,8 @@
 //   { age: 10, name: "Юля" },
 //   { age: 20, name: "Катя" },
 // ];
+
+// let ageArr = girls.map((girl) => girl.age);
+// console.log(ageArr);
+// let aveAge = ageArr.reduce((sum, age) => sum + age) / ageArr.length;
+// console.log(aveAge);

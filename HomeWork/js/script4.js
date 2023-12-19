dc239440fa938e9998d0386419ffae98716ac37b
Tetiana Deineka <tata4ka666@i.ua>
2023-12-19 22:27:03 +0200

    Update script4.js

--- HomeWork/js/script4.js
+++ HomeWork/js/script4.js
@@ -8,8 +8,24 @@
 //     }
 // }
 
+// function checkAge(age) {
+//     age > 18 ? "true" : confirm("Батьки дозволили?");
+// }
+// console.log(checkAge(17));
+// console.log(checkAge(22));
+
 // ---2---
 // Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b.
+
+// function min(a, b) {
+//     if (a < b) {
+//         return a;
+//     }
+//     return b;
+// }
+// console.log(min(4, 2));
+// console.log(min(-1, 3));
+
 
 // ---3---
 // Перепишіть з використанням стрілкових функцій
@@ -19,6 +35,15 @@
 //     else no();
 // }
 // ask("Ви згодні?",
+//     function() { alert("Ви погодились."); },
+//     function() { alert("Ви скасували виконання."); }
+// );
+
+// const ask = (question, yes, no) => {
+//   if (confirm(question)) yes();
+//   else no();
+// };
+// ask ("Ви згодні?",
 //     function() { alert("Ви погодились."); },
 //     function() { alert("Ви скасували виконання."); }
 // );

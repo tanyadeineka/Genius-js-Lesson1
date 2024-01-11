7f9eef85ceb2ed8451c41cdf48ed614974bc29f1
Tetiana Deineka <tata4ka666@i.ua>
2024-01-11 16:56:02 +0200

    Create script6.js

---
+++ HomeWork/js/script6.js
@@ -0,0 +1,26 @@
+// Створити телефонну книгу:
+// * створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// * створити set який прийматиме телефон і номер
+// * створити get який виводитиме данні про абонента
+// * створити три різних юзери
+// * вивести данні
+
+// class Abonent {
+//     constructor(name, number) {
+//         this.name = name;
+//         this.number = number;
+//     }
+//     set phoneNumber(newNumber) {
+//         this.number = newNumber;
+//     }
+//     get info() {
+//         return `Abonent: ${this.name}, phone number: ${this.number}`;
+//     }
+// }
+// const Anna = new Abonent('Anna', '+380505556677');
+// const Dan = new Abonent('Dan', '+380604445566');
+// const Mila = new Abonent('Mila', '+380203334455');
+
+// console.log(Anna);
+// console.log(Dan);
+// console.log(Mila);

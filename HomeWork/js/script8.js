5804ef66509e7d26f38b52916e7d68a7493a611f
Tetiana Deineka <tata4ka666@i.ua>
2024-01-16 17:03:51 +0200

    Create script8.js

---
+++ HomeWork/js/script8.js
@@ -0,0 +1,13 @@
+// Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з
+// документа та прочитати його значення.
+//
+// < !DOCTYPE html >
+// <html>
+// <body>
+// <div data-widget-name="menu">Виберіть жанр</div>
+// <script>/* ваш код */</script>
+// </body>
+// </html>
+
+// const menu = document.querySelector('[data-widget-name="menu"]');
+// console.log(menu.textContent);

601e7e0bd91735bef59c2af53bef7cabf49e08a3
Tetiana Deineka <108138671+tanyadeineka@users.noreply.github.com>
2024-01-31 18:17:21 +0200

    Update script9.js

--- HomeWork/js/script9.js
+++ HomeWork/js/script9.js
@@ -2,6 +2,11 @@
 // Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент
 // <div id="text"> зникав.
 
+const text = document.querySelector('#text');
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', ()=> text.style.display = none);
+
+
 // -----2----
 // Напишіть такий код, щоб після натискання на кнопку, вона зникала.
 

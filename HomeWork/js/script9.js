e6cb7fce841a425e650fe949944af44b8a751b2e
Tetiana Deineka <108138671+tanyadeineka@users.noreply.github.com>
2024-01-31 22:11:42 +0200

    Update script9.js

--- HomeWork/js/script9.js
+++ HomeWork/js/script9.js
@@ -2,13 +2,35 @@
 // Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент
 // <div id="text"> зникав.
 
-// const text = document.querySelector('#text');
-// const btn = document.querySelector('.btn');
-// btn.addEventListener('click', ()=> text.style.display = none);
+/* function hideElement() {
+    const textElement = document.getElementById('text');
+    if(textElement) {
+        textElement.style.display = "none";
+    }
+} */ 
 
 
 // -----2----
 // Напишіть такий код, щоб після натискання на кнопку, вона зникала.
 
+/* function hideElement() {
+    const btn = document.getElementById('btn');
+    btn.style.display = "none";
+} */
+
 // -----3----
 // Створіть дерево, яке показує/приховує дочірні вузли при кліці
+
+/* const tree = document.querySelector(".tree");
+
+tree.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  const checkClass = tree.querySelector("li.active");
+
+  if (checkClass) {
+    event.target.classList.remove("active");
+  } else {
+    event.target.classList.add("active");
+  }
+} */
